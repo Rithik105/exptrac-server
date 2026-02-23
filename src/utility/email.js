6b778bf2872c552f9ee6815ac00fa0853d17ddba
Rithik raj <rithikraj299@neel-tech.international>
2026-02-23 17:15:57 +0530

    nodemailer fix 2

--- src/utility/email.js
+++ src/utility/email.js
@@ -4,7 +4,6 @@ const mailer = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 587,
   secure: false,
-  service: "gmail",
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
