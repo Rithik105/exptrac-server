9243126c8127c0910402fc76bdf33c9a1e55026e
Rithik raj <rithikraj299@neel-tech.international>
2026-02-23 19:10:40 +0530

    brevo implementation
    replaced nodemail with new brevo client

--- src/utility/email.js
+++ src/utility/email.js
@@ -1,9 +1,9 @@
+const { BrevoClient } = require("@getbrevo/brevo");
+
 const nodemailer = require("nodemailer");
 
 const mailer = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 587,
-  secure: false,
+  service: "gmail",
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
@@ -16,11 +16,12 @@ const verify = async (req, res) => {
     return res.status(200).send("SMTP CONNECTION OK");
   } catch (err) {
     console.error("SMTP VERIFY ERROR:", err);
+    res.status(200).send(err);
     return false;
   }
 };
 
-const sendEmail = async (to, otp) => {
+const sendGoogleEmail = async (to, otp) => {
   const subject = "Password Reset";
   const html = `<div style="
     font-family: Arial, sans-serif;
@@ -74,6 +75,73 @@ const sendEmail = async (to, otp) => {
     to,
     subject,
     html,
+  });
+};
+
+const brevo = new BrevoClient({
+  apiKey: process.env.BREVO_API_KEY,
+});
+
+const sendEmail = async (toEmail, otp) => {
+  const subject = "Password Reset";
+  const html = `<div style="
+    font-family: Arial, sans-serif;
+    background-color: #f4f6f8;
+    padding: 40px 20px;
+  ">
+    <div style="
+      max-width: 500px;
+      margin: auto;
+      background: white;
+      border-radius: 8px;
+      padding: 30px;
+      box-shadow: 0 4px 10px rgba(0,0,0,0.05);
+      text-align: center;
+    ">
+      
+      <h2 style="color: #333; margin-bottom: 10px;">
+        Password Reset Request
+      </h2>
+
+      <p style="color: #555; font-size: 14px;">
+        Use the OTP below to reset your password.
+      </p>
+
+      <div style="
+        margin: 30px 0;
+        font-size: 32px;
+        font-weight: bold;
+        letter-spacing: 8px;
+        color: #95d14c;
+      ">
+        ${otp}
+      </div>
+
+      <p style="color: #777; font-size: 13px;">
+        This code expires in 10 minutes.
+      </p>
+
+      <hr style="margin: 30px 0; border: none; border-top: 1px solid #eee;" />
+
+      <p style="font-size: 12px; color: #999;">
+        If you didn’t request a password reset, you can safely ignore this email.
+      </p>
+
+    </div>
+  </div>
+`;
+  await brevo.transactionalEmails.sendTransacEmail({
+    sender: {
+      email: process.env.BREVO_FROM_EMAIL,
+      name: "exptrac",
+    },
+    subject: subject,
+    htmlContent: html,
+    to: [
+      {
+        email: toEmail,
+      },
+    ],
   });
 };
 
